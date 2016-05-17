b92c08e2ce885d99d984011fac4a8db909237deb
sander <s.teunissen@gmail.com>
2016-05-17 13:00:14 +0200

    added argparser

--- nordic.py
+++ nordic.py
@@ -1,12 +1,17 @@
 from serial import Serial
+import argparse
 
 COMMANDS = {
     "open": b'\x00\x03RU\x00',
     "close": b'\x00\x03RD\x00'
 }
 
+parser = argparse.ArgumentParser()
+parser.add_argument("serialport",help="provide a serial port.")
+
 if __name__ == "__main__":
-    SERIAL_PORT = "COM2"
+    args = parser.parse_args()
+    SERIAL_PORT = args.serialport
     SERIAL_SPEED = 38400
     s = Serial(SERIAL_PORT, SERIAL_SPEED)
     commands = {"o": ("open",COMMANDS["open"]),
@@ -25,3 +30,4 @@ if __name__ == "__main__":
         val = commands.get(inp)
         if val is not None:
             s.write(val[1])
+    s.close()
