63f8474cfbab53a8fbd011144f34a422ca7a1af9
sander <s.teunissen@gmail.com>
2016-05-17 14:28:57 +0200

    added argparser

--- helpers.py
+++ helpers.py
@@ -1,15 +1,18 @@
 from aiohttp import web
-
+import asyncio
 
 def view_factory(url, path):
     """
     Factory method to serve a single
     static file.
     """
-    async def static_view(request):
+
+    @asyncio.coroutine
+    def static_view(request):
         prefix = url.rsplit('/', 1)[0] or '/'
         route = web.StaticRoute(None, prefix, '')
         request.match_info['filename'] = path
-        return await route.handle(request)
+        val = yield from route.handle(request)
+        return val
 
     return static_view
