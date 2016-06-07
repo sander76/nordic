40216e05717dd312356e737714ab8920e9bfc46d
sanders76 <s.teunissen@gmail.com>
2016-06-07 13:00:52 +0200

    basics implemented

--- validation.py
+++ validation.py
@@ -7,67 +7,76 @@ from nordic import COMMANDS
 cmds = COMMANDS.keys()
 
 schema = {'products':
-              {'type': 'list',
+              {'type':
+                   'list',
                'schema':
-                   {'type': 'dict',
+                   {'type':
+                        'dict',
                     'schema':
-                        {'title': {'type': 'string'},
-                         'steps': {'type': 'list',
-                                   'schema': {'type': 'dict',
-                                              'schema': {'title': {'type': 'string'},
-                                                         'instructions': {'type': 'list',
-                                                                          'schema': {'oneof': [
-                                                                              {'type': 'string'},
-                                                                              {'type': 'dict',
-                                                                               'schema': {'type': {'type': 'string',
-                                                                                                   'allowed': ['button']
-                                                                                                   },
-                                                                                          'caption': {'type': 'string'},
-                                                                                          'commands': {'type': 'dict',
-                                                                                                       'schema': {
-                                                                                                           'commands': {
-                                                                                                               'type': 'list',
-                                                                                                               'allowed': cmds},
-                                                                                                           'delay': {
-                                                                                                               'type': 'integer'}
-                                                                                                       }
-                                                                                                       },
-                                                                                          'keys': {'type': 'string'},
-                                                                                          'confirm': {'type': 'boolean'}
-                                                                                          }
-                                                                               },
-                                                                              {'type': 'dict',
-                                                                               'schema': {'type': {'type': 'string',
-                                                                                                   'allowed': ['remote']
-                                                                                                   }
-                                                                                          }
-                                                                               },
-                                                                              {'type': 'dict',
-                                                                               'schema': {'type': {'type': 'string',
-                                                                                                   'allowed': [
-                                                                                                       'navbutton']
-                                                                                                   },
-                                                                                          'caption': {'type': 'string'},
-                                                                                          'goto': {'type': 'integer'}}},
-                                                                              {'type': 'dict',
-                                                                               'schema': {'type': {'type': 'string',
-                                                                                                   'allowed': [
-                                                                                                       'image']},
-                                                                                          'src': {'type': 'string'}}}
+                        {'title':
+                             {'type': 'string'},
+                         'steps':
+                             {'type': 'list',
+                              'schema':
+                                  {'type': 'dict',
+                                   'schema': {'title': {'type': 'string'},
+                                              'instructions': {'type': 'list',
+                                                               'schema': {'oneof': [
+                                                                   {'type': 'string'},
+                                                                   {'type': 'dict',
+                                                                    'schema': {'type': {'type': 'string',
+                                                                                        'allowed': ['button'],
+                                                                                        'required': True
+                                                                                        },
+                                                                               'caption': {'type': 'string'},
+                                                                               'commands': {'type': 'dict',
+                                                                                            'schema': {
+                                                                                                'commands': {
+                                                                                                    'type': 'list',
+                                                                                                    'allowed': cmds,
+                                                                                                    'required': True
+                                                                                                },
 
-                                                                          ]}
-                                                                          },
-                                                         'buttons': {'type': 'list'},
-                                                         'confirm': {'type': 'dict',
-                                                                     'schema': {'img': {'type': 'string'},
-                                                                                'text': {'type': 'string'},
-                                                                                'next': {'type': 'integer'},
-                                                                                'previous': {'type': 'integer'}}},
-                                                         'next': {'type': ['boolean', 'string']},
-                                                         'previous': {'type': ['boolean', 'string']}
-                                                         }
+                                                                                                'delay': {
+                                                                                                    'type': 'integer'}
+                                                                                            }
+                                                                                            },
+                                                                               'keys': {'type': 'string'},
+                                                                               'confirm': {'type': 'boolean'}
+                                                                               }
+                                                                    },
+                                                                   {'type': 'dict',
+                                                                    'schema': {'type': {'type': 'string',
+                                                                                        'allowed': ['remote']
+                                                                                        }
+                                                                               }
+                                                                    },
+                                                                   {'type': 'dict',
+                                                                    'schema': {'type': {'type': 'string',
+                                                                                        'allowed': [
+                                                                                            'navbutton']
+                                                                                        },
+                                                                               'caption': {'type': 'string'},
+                                                                               'goto': {'type': 'integer'}}},
+                                                                   {'type': 'dict',
+                                                                    'schema': {'type': {'type': 'string',
+                                                                                        'allowed': [
+                                                                                            'image']},
+                                                                               'src': {'type': 'string'}}}
+
+                                                               ]}
+                                                               },
+                                              'buttons': {'type': 'list'},
+                                              'confirm': {'type': 'dict',
+                                                          'schema': {'img': {'type': 'string'},
+                                                                     'text': {'type': 'string'},
+                                                                     'next': {'type': 'integer'},
+                                                                     'previous': {'type': 'integer'}}},
+                                              'next': {'type': ['boolean', 'string']},
+                                              'previous': {'type': ['boolean', 'string']}
                                               }
                                    }
+                              }
                          }
                     }
                }
@@ -76,7 +85,7 @@ schema = {'products':
 if __name__ == "__main__":
     v = Validator(schema)
 
-with open('static/app/instructions/instruction-test.json') as fl:
+with open('static/app/instructions/instructions-nl.json') as fl:
     dta = json.load(fl)
 val = v.validate(dta)
 print(val)
