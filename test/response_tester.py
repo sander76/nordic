4e6ca5663775ad708b91d92c0c89b55046b285c5
sanders76 <s.teunissen@gmail.com>
2017-02-21 13:34:17 +0100

    looper..

--- test/response_tester.py
+++ test/response_tester.py
@@ -1,6 +1,7 @@
 # serial = nordic_serial.NordicSerial(app.loop, SERIAL_PORT, SERIAL_SPEED, network_id)
 import asyncio
 import logging
+import argparse
 
 from server.messenger import Messengers
 
@@ -12,32 +13,38 @@ from server.nordic_serial import NordicSerial
 
 
 def keys():
-    _keys = (server.nordic.OPEN, server.nordic.CLOSE, server.nordic.STOP)
+    _keys = (server.nordic.OPEN, server.nordic.CLOSE, server.nordic.STOP,
+             b'\x00\x03RU')
     i = 0
     for _key in _keys:
         yield _key
 
 
 @asyncio.coroutine
-def looper(loop):
+def looper(loop, serial_port):
     loops = 1
     messengers = Messengers(loop)
-    serial = NordicSerial(loop, "COM8", SERIAL_SPEED, network_id,
+    serial = NordicSerial(loop, serial_port, SERIAL_SPEED, network_id,
                           messengers=messengers)
     while loops < 50:
         loops += 1
         for key in keys():
             upstring = COMMANDS[key]
             yield from serial.send_queue.put(upstring)
-            #print("queue size: {}".format(serial.send_queue.qsize()))
-            yield from asyncio.sleep(2)
+            # print("queue size: {}".format(serial.send_queue.qsize()))
+            yield from asyncio.sleep(1)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--serialport")
+    args = parser.parse_args()
+    SERIAL_PORT = args.serialport
+
     logging.basicConfig(level=logging.DEBUG)
     network_id = get_id()
     loop = asyncio.get_event_loop()
 
     # loop.create_task(looper(serial))
-    loop.run_until_complete(looper(loop))
+    loop.run_until_complete(looper(loop, SERIAL_PORT))
     # loop.run_forever()
