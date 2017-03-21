90c73ae792d7e716fb3ad165361c670fd06cd1e5
sanders76 <s.teunissen@gmail.com>
2017-03-21 11:31:20 +0100

    adding status route

--- server/webserver/routes.py
+++ server/webserver/routes.py
@@ -1,4 +1,5 @@
-from server.webserver.views import instruction_handler, websocket_handler
+from server.webserver.views import instruction_handler, websocket_handler, \
+    get_connection_status
 
 
 def add_routes(app, serial):
@@ -7,3 +8,4 @@ def add_routes(app, serial):
     app.router.add_static("/app/", "static/app/")
     app.router.add_route("GET", '/instructions/{lang}', instruction_handler)
     app.router.add_route('GET', '/ws', websocket_handler)
+    app.router.add_route("GET","/serialstatus/status",get_connection_status)
