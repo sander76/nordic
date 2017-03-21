15c80f33e4faf6cb01cd18d4304120db0e0bd7b1
sanders76 <s.teunissen@gmail.com>
2017-03-21 16:02:25 +0100

    add german

--- instructor/translations.py
+++ instructor/translations.py
@@ -4,93 +4,77 @@ HANGPRODUCT = TXT(
     "HANG PRODUCT.",  # English language.
     "PRODUCT OPHANGEN.",  # Dutch language.
     il=None,  # Israeli language.
-    de=None,  # German language
+    de="Produkt aufhängen",  # German language
     dk=None)  # Danish language
 
 HANG_TWIST = TXT(
     "HANG THE TWIST BLIND",  # English language.
     "HANG DE TWIST BLIND OP",  # Dutch language.
     il=None,
-    de=None,
+    de="Hängen Sie das Doppelrollo auf",
     dk=None)
 
 HANG_RB = TXT(
     "HANG THE ROLLERBLIND",
     "HANG HET ROLGORDIJN OP",
     il=None,
-    de=None,
+    de="Hängen Sie das Rollo auf",
     dk=None)
 
 CONNECT = TXT(
     "CONNECT.",
     "VERBINDEN.",
     il=None,
-    de=None,
+    de="Verbinden",
     dk=None)
 
 INITIALISE = TXT(
     "INIT.",
     "INITALISEREN.",
     il=None,
-    de=None,
+    de="Initialisieren",
     dk=None)
 
 ENTER_PROGRAM_MODE = TXT(
     "ENTER PROGRAM MODE.",
     "PROGRAMMEER MODUS.",
     il=None,
-    de=None,
+    de="Programmiermodus",
     dk=None)
 
 BLIND_DIRECTION = TXT(
     "CHECK BLIND DIRECTION.",
     "CONTROLEER BLIND RICHTING.",
     il=None,
-    de=None,
+    de="Kontrollieren Sie die Laufrichtung",
     dk=None)
 
 LEFT_MOUNT = TXT(
     "MOTOR ON THE LEFT",
-    "MOTOR LINKS")
-
-# Product orientation questions.
+    "MOTOR LINKS",
+    de="Motor Links")
 
 IS_LEFT_MOUNT = TXT(
     "Is the motor on the **left** ?",
-    "Is de motor **links** gemonteerd ?")
+    "Is de motor **links** gemonteerd ?",
+    de="Ist der Motor links montiert?")
 
 IS_RIGHT_MOUNT = TXT(
     "Is the motor on the **right** ?",
-    "Is de motor **rechts** gemonteerd ?")
+    "Is de motor **rechts** gemonteerd ?",
+    de="Ist der Motor rechts montiert?")
 
 RIGHT_MOUNT = TXT(
     "MOTOR ON THE RIGHT",
-    "MOTOR RECHTS")
-
-ORIENT_VVB_BACK_TITLE = TXT(
-    "Is the motor mounted on the **back** ?",
-    "Is the motor **achter** gemonteerd ?"
-)
-
-ORIENT_VVB_UPRIGHT_TITLE = TXT(
-    "Is the motor mounted **above** ?",
-    "Is de motor **boven** gemonteerd ?"
-)
-
-ORIENT_VVB_BACK = TXT(
-    "MOTOR ON THE BACK",
-    "MOTOR ACHTER"
-)
-ORIENT_VVB_UPRIGHT = TXT(
-    "MOTOR ABOVE",
-    "MOTOR BOVEN"
-)
+    "MOTOR RECHTS",
+    de="Motor rechts")
 
 BACKROLLER_LEFT_TITLE = TXT(
     "LEFT BACKROLLER", "LINKS BACKROLLER",
     il=None,
-    de=None,
+    de="links, hinten abrollend",
     dk=None)
+
 BACKROLLER_RIGHT_TITLE = TXT(
     "RIGHT BACKROLLER", "RECHTS BACKROLLER",
     il=None,
@@ -99,252 +83,252 @@ BACKROLLER_RIGHT_TITLE = TXT(
 FRONTROLLER_LEFT_TITLE = TXT(
     "LEFT FRONTROLLER", "LINKS FRONTROLLER",
     il=None,
-    de=None,
+    de="links, vorne abrollend",
     dk=None)
 FRONTROLLER_RIGHT_TITLE = TXT(
     "RIGHT FRONTROLLER", "RECHTS FRONTROLLER",
     il=None,
-    de=None,
+    de="rechts, vorne abrollend",
     dk=None)
 
 IS_LEFT_BACKROLLER = TXT(
     "Is the motor on the **left** and **backroller** ?",
     "Is de motor **links** gemonteerd en **backroller** ?",
     il=None,
-    de=None,
+    de="Ist der Motor links montiert hinten abrollend?",
     dk=None)
 
 IS_RIGHT_BACKROLLER = TXT(
     "Is the motor on the **right** and **backroller** ?",
     "Is de motor **rechts** gemonteerd en **backroller** ?",
     il=None,
-    de=None,
+    de="Ist der Motor rechts montiert und hinten abrollend?",
     dk=None)
 
 IS_LEFT_FRONTROLLER = TXT(
     "Is the motor on the **left** and **frontroller** ?",
     "Is de motor **links** gemonteerd en **contrarollend** ?",
     il=None,
-    de=None,
+    de="ist der Motor links montiert und vorne abrollend?",
     dk=None)
 
 IS_RIGHT_FRONTROLLER = TXT(
     "Is the motor on the **right** and **frontroller** ?",
     "Is de motor **rechts** gemonteerd en **contrarollend** ?",
     il=None,
-    de=None,
+    de="ist der Motor rechts montiert und vorne abrollend?",
     dk=None)
 
 SWITCH_DIRECTION = TXT(
     "CHANGE BLIND DIRECTION.",
     "WISSEL BLIND RICHTING.",
     il=None,
-    de=None,
+    de="Laufrichtung ändern",
     dk=None)
 
 SET_BOTTOM_LIMIT = TXT(
     "SET BOTTOM LIMIT.",
     "STEL ONDERLIMIET IN.",
     il=None,
-    de=None,
+    de="Untere Endlageneinstellung",
     dk=None)
 
 SET_TOP_LIMIT = TXT(
     "SET TOP LIMIT.",
     "STEL BOVENLIMIET IN.",
     il=None,
-    de=None,
+    de="obere Endlageneinstellung",
     dk=None)
 
 IS_BLIND_AT_TOP = TXT(
     "IS THE BLIND AT THE TOP ?",
     "IS DE BLIND HELEMAAL BOVEN ?"
-)
+    "ist die Anlage hochgefahren?")
 
 SET_TWIST_SLAT_POSITION = TXT(
     "SET TWIST SLAT POSITION.",
     "STEL TWIST SLAT POSITIE IN.",
     il=None,
-    de=None,
+    de="einstellen der Lamellenposition des Doppelrollos",
     dk=None)
 
 SKIP_TOP_TITLE = TXT(
     "SET *TOP* LIMIT?",
     "STEL *BOVENLIMIET* IN?",
     il=None,
-    de=None,
+    de="Obere Endlage einstellen?",
     dk=None)
 
 SKIP_BOTTOM_TITLE = TXT(
     "SET *BOTTOM* LIMIT?",
     "STEL *ONDERLIMIET* IN?",
     il=None,
-    de=None,
+    de="Untere Endlage einstellen?",
     dk=None)
 
 SKIP_SLAT_TITLE = TXT(
     "SET SLAT POSITION?",
     "STEL SLAT POSITIE IN?",
     il=None,
-    de=None,
+    de="Lamellenposition einstellen?",
     dk=None)
 
 PROPER_PRODUCT_HANG = TXT(
     "Make sure *BOTTOM* bar is approximately 20cm down.",
     "Zorg dat de *ONDERLAT* ongeveer 20cm gezakt is.", il=None,
-    de=None,
+    de="sicherstellen, dass die Unterschiene ungefähr 20cm unten ist",
     dk=None)
 
 PROPER_PRODUCT_HANG_CONFIRM = TXT(
     "Product fixed *OK*.",
     "Product is correct opgehangen.",
     il=None,
-    de=None,
+    de="Produkt is korrekt montiert",
     dk=None)
 
 MAKE_CHOICE = TXT(
     "Make a choice:",
     "Maak een keuze:",
     il=None,
-    de=None,
+    de="auswählen",
     dk=None)
 
 SELECT_SKIP_TOP = TXT(
     "I don't want to set the TOP limit",
     "BOVENLIMIET niet opnieuw instellen",
     il=None,
-    de=None,
+    de="Obere Endlage nicht einstellen",
     dk=None)
 
 SELECT_SKIP_BOTTOM = TXT(
     "I don't want to set the *BOTTOM* limit",
     "*ONDERLIMIET* niet opnieuw instellen",
     il=None,
-    de=None,
+    de="untere Endlage nicht einstellen",
     dk=None)
 
 SELECT_SKIP_SLAT = TXT(
     "Don't re-set the *SLAT* position.",
     "*SLAT OPEN* positie niet opnieuw instellen.",
     il=None,
-    de=None,
+    de="Lamellenposition nicht erneut festlegen",
     dk=None)
 
 RESET_TOP = TXT(
     "Re-set the TOP limit.",
     "BOVENLIMIET opnieuw instellen.",
     il=None,
-    de=None,
+    de="Obere Endlage erneut festlegen",
     dk=None)
 
 START_TOP_PROGRAMMING = TXT(
     "The blind will move completely to the top.",
     "De blind zal helemaal naar boven bewegen."
-)
+    "die Anlage wird ganz nach oben fahren")
 
 RESET_BOTTOM = TXT(
     "Re-set the BOTTOM limit.",
     "ONDERLIMIET opnieuw instellen.",
     il=None,
-    de=None,
+    de="Untere Endlage erneut festlegen",
     dk=None)
 
 RESET_SLAT = TXT(
     "Re-set the SLAT position",
     "SLAT positie opnieuw instellen.",
     il=None,
-    de=None,
+    de="Lamellenposition erneut festlegen",
     dk=None)
 
 TEST_BLINDS = TXT(
     "TEST BLINDS",
     "TEST BLINDS",
     il=None,
-    de=None,
+    de="Anlagentest",
     dk=None)
 
 DID_THE_MOTOR_JOG = TXT(
     "Did the motor jog?",
     "Heeft de motor kort bewogen?",
     il=None,
-    de=None,
+    de="hat sich der Motor kurz bewegt?",
     dk=None)
 
 DID_THE_MOTOR_MOVE_UP = TXT(
     "Did the blind move to the top?",
     "Is de blind naar boven bewogen?",
     il=None,
-    de=None,
+    de="Ist die Anlage hochgefahren?",
     dk=None)
 
 DID_THE_MOTOR_JOG_TWO_TIMES = TXT(
     "Did the motor jog two times?",
     "Heeft de motor twee keer bewogen?",
     il=None,
-    de=None,
+    de="hat sich der Motor zweimal kurz bewegt?",
     dk=None)
 
 DID_MOTOR_MOVE_DOWN = TXT(
     "Did the blind move down?",
     "Is de blind naar beneden bewogen?",
     il=None,
-    de=None,
+    de="Ist die Anlage runtergefahren?",
     dk=None)
 
 PRESS_HOLD_BLIND_BUTTON = TXT(
     "`Press` and `hold` the *BLIND BUTTON* .",
     "`Druk` de *BLIND BUTTON*  in en `houd vast`.",
     il=None,
-    de=None,
+    de="Drücken und halten Sie die Motortaste",
     dk=None)
 
 KEEP_PRESSING_AND_OKAY = TXT(
     "Keep pressing the *BLIND BUTTON* and press the OK button.",
     "Blijf de *BLIND BUTTON* indrukken en druk op de OK knop.",
     il=None,
-    de=None,
+    de="Motortaste gedrückt halten und die OK Taste drücken",
     dk=None)
 
 RELEASE_THE_BLIND_BUTTON = TXT(
     "Release the *BLIND BUTTON*",
     "Laat de *BLIND BUTTON* los.",
     il=None,
-    de=None,
+    de="Motortaste loslassen",
     dk=None)
 
 PRESS_OKAY_BUTTON = TXT(
     "Press OK button",
     "Druk op de OK knop",
     il=None,
-    de=None,
+    de="OK Taste drücken",
     dk=None)
 
 WATCH_THE_BLIND_JOG = TXT(
     "Watch the *BLIND* jog.",
     "Zie de *BLIND* bewegen.",
     il=None,
-    de=None,
+    de="gucken Sie, ob die Anlage sich bewegt",
     dk=None)
 
 WATCH_THE_BLIND_MOVE_UP = TXT(
     "Watch the *BLIND* move up.",
     "Zie de *BLIND* naar boven bewegen.",
     il=None,
-    de=None,
+    de="Gucken Sie, ob die Anlage hochfährt",
     dk=None)
 
 WATCH_THE_BLIND_JOG_TWO_TIMES = TXT(
     "Motor jogs two times.",
     "Motor beweegt twee keer.",
     il=None,
-    de=None,
+    de="Motor bewegt sich zweimal kurz",
     dk=None)
 
 MOTOR_SHOULD_MOVE_DOWN = TXT(
     "Blind should move down/close.",
     "Blind moet naar beneden bewegen/sluiten.",
     il=None,
-    de=None,
+    de="Anlage sollte herunterfahren/schließen",
     dk=None
 )
 
@@ -352,80 +336,80 @@ MOVE_BLIND_BOTTOM = TXT(
     "Move the *BLIND* to the desired BOTTOM position.",
     "Stuur de *BLIND* naar de gewenste ONDER positie.",
     il=None,
-    de=None,
+    de="Bewegen Sie die Anlage auf die gewünschte untere Endlage",
     dk=None)
 
 MOVE_BLIND_TOP = TXT(
     "Move the *BLIND* to the desired TOP position.",
     "Stuur de *BLIND* naar de gewenste BOVEN positie.",
     il=None,
-    de=None,
+    de="Bewegen Sie die Anlage in die gewünschte obere Endlage",
     dk=None)
 
 MOVE_BLIND_SLAT_OPEN = TXT(
     "Move the *BLIND* to the desired SLAT OPEN position.",
     "Stuur de *BLIND* naar de gewenste SLAT OPEN positie.",
     il=None,
-    de=None,
+    de="Bewegen Sie die Lamellen in die gewünschte Position",
     dk=None)
 
 TEST_MOVE_BLINDS = TXT(
     "Check TOP and BOTTOM limits.",
     "Controleer BOVEN en ONDER limiet.",
     il=None,
-    de=None,
+    de="Kontrollieren Sie obere und untere Endlage",
     dk=None)
 
 SAVE_BOTTOM = TXT(
     "Save this as BOTTOM limit.",
     "Stel in als ONDER positie.",
     il=None,
-    de=None,
+    de="Speichern als untere Endlage",
     dk=None)
 
 SAVE_TOP = TXT(
     "Save this as TOP limit.",
     "Stel in als BOVEN positie.",
     il=None,
-    de=None,
+    de="Speichern als obere Endlage",
     dk=None)
 
 SAVE_THIS_AS_TOP = TXT(
     "Save this as the TOP limit?",
     "Deze positie als BOVEN limiet instellen?"
-)
+    "Diese Position als obere Endlage einstellen?)")
 
 SAVE_SLAT = TXT(
     "Save this as SLAT OPEN position.",
     "Stel in als SLAT OPEN positie.",
     il=None,
-    de=None,
+    de="Diese Lamellenposition als OFFEN sicher",
     dk=None)
 
 LIMITS_OK = TXT(
     "Limits are OK. Do a new blind.",
     "Limieten zijn OK. Nieuwe *BLIND* inprogrammeren.",
     il=None,
-    de=None,
+    de="Endlagen sind ok. Neue Anlage programmieren",
     dk=None)
 
 LIMITS_NOT_OK = TXT(
     "Limits are NOT OK. Re-set them",
     "Limieten zijn NIET OK. Opnieuw instellen.",
     il=None,
-    de=None,
+    de="Endlagen sind nicht OK. Erneut einstellen",
     dk=None)
 
 YES = TXT(
     "yes",
     "ja",
     il=None,
-    de=None,
+    de="ja",
     dk=None)
 
 NO = TXT(
     "no",
     "nee",
     il=None,
-    de=None,
+    de="nein",
     dk=None)
