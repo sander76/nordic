00869feb6f7756b9f7787d007ee148fab2d657af
sanders76 <s.teunissen@gmail.com>
2017-05-29 16:24:11 +0200

    testing

--- scripts/server_alt.sh
+++ scripts/server_alt.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 HOME=/home/ecmenc
+
 VENVDIR=$HOME/venv_nordic/
 BINDIR=$HOME/nordic
 
-source $VENVDIR/bin/activate
+cd $VENVDIR
+source bin/activate
 cd $BINDIR
 python new_server.py --serialport="/dev/ttyUSB0"
