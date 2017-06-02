9e06882e4f7bd0f4c2533c85560223fbf688ff0b
sanders76 <s.teunissen@gmail.com>
2017-06-02 14:27:11 +0200

    fix instructions path ... again

--- server/webserver/routes.py
+++ server/webserver/routes.py
@@ -6,7 +6,7 @@ from server.webserver.views import instruction_handler, websocket_handler, \
 def add_routes(app, serial, static_folder, instructions_folder):
     # create some routes.
     app.router.add_route('POST', '/nordic', serial.send_nordic)
-    app.router.add_static("/app/instructions/", instructions_folder)
+    app.router.add_static("/instructions/", instructions_folder)
     app.router.add_static("/app/", static_folder)
     # app.router.add_route("GET", '/instructions/{lang}', instruction_handler)
     app.router.add_route('GET', '/ws', websocket_handler)
