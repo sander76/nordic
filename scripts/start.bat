ba5ed97bdb20a850607659530aa69a5205c0079e
sanders76 <s.teunissen@gmail.com>
2017-06-09 11:23:53 +0200

    another script update1

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,5 +1,6 @@
 call git pull
 :: assuming the virtualenv is located one folder higher
-call ..\nordic-venv\scripts\activate.bat
+call ..\..\nordic-venv\scripts\activate.bat
+cd ..
 
 python new_server.py
