8bc2f205920924fbc2ea0fec1df68c0b62d9f14e
sander <s.teunissen@gmail.com>
2018-04-24 17:03:43 +0200

    cleaning up

--- scripts/create_startup_file.py
+++
@@ -1,9 +0,0 @@
-# creates a windows startup file.
-
-with open("startup.bat", 'w') as fl:
-    fl.write('cd..\n')
-    fl.write('call git pull\n')
-
-    fl.write('call env\\scripts\\activate.bat\n')
-    serial_port = input("Provide serial port of dongle: ")
-    fl.write('python new_server.py --serialport="{}"\n'.format(serial_port))
