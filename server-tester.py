f82effd0894296b30115c2b0971ed59043e5aab3
sander <s.teunissen@gmail.com>
2019-01-18 11:12:08 +0100

    loop to 50

--- server-tester.py
+++ server-tester.py
@@ -29,39 +29,38 @@ delays = iter(_delays())
 
 
 async def w_socket(session):
-    async with session.ws_connect(
-        "ws://localhost:8080/ws"
-    ) as ws:
+    async with session.ws_connect("ws://localhost:8080/ws") as ws:
         async for msg in ws:
             if msg.type == aiohttp.WSMsgType.ERROR:
                 break
             else:
                 print(msg.data)
 
+
 async def sender(session):
-    loop=10
+    loop = 50
     for i in range(loop):
         for command in commands:
             async with session.post(
-                    "http://localhost:8080/nordic", json={"commands": command}
+                "http://localhost:8080/nordic", json={"commands": command}
             ) as resp:
                 txt = await resp.text()
                 print(txt)
             await asyncio.sleep(next(delays))
+
 
 async def looper(loop):
 
     async with aiohttp.ClientSession() as session:
         _ws = loop.create_task(w_socket(session))
         _sender = loop.create_task(sender(session))
-        #await _ws
+        # await _ws
         await _sender
 
         await session.close()
 
+
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-
-
 
     loop.run_until_complete(looper(loop))
