962b8e2332ff50ffa67c51fa76dd528393c06202
sander <s.teunissen@gmail.com>
2019-01-18 11:07:14 +0100

    adding websocket

--- server-tester.py
+++ server-tester.py
@@ -28,19 +28,40 @@ def _delays():
 delays = iter(_delays())
 
 
-async def looper():
-    loop = 50
+async def w_socket(session):
+    async with session.ws_connect(
+        "ws://localhost:8080/ws"
+    ) as ws:
+        async for msg in ws:
+            if msg.type == aiohttp.WSMsgType.ERROR:
+                break
+            else:
+                print(msg.data)
+
+async def sender(session):
+    loop=10
+    for i in range(loop):
+        for command in commands:
+            async with session.post(
+                    "http://localhost:8080/nordic", json={"commands": command}
+            ) as resp:
+                txt = await resp.text()
+                print(txt)
+            await asyncio.sleep(next(delays))
+
+async def looper(loop):
 
     async with aiohttp.ClientSession() as session:
-        for i in range(loop):
-            for command in commands:
-                async with session.post(
-                    "http://localhost:8080/nordic", json={"commands": command}
-                ) as resp:
-                    print(resp.text())
-                await asyncio.sleep(next(delays))
+        _ws = loop.create_task(w_socket(session))
+        _sender = loop.create_task(sender(session))
+        #await _ws
+        await _sender
 
+        await session.close()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(looper())
+
+
+
+    loop.run_until_complete(looper(loop))
